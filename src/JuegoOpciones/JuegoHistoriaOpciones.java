920fbf1de40f9a5a01f9dc09a3df4f711ccad20f
Gabriel Canda <gabriel.candagcb9@gmail.com>
2021-03-15 08:56:40 +0100

    Creacion del main del juego de opciones

---
+++ src/JuegoOpciones/JuegoHistoriaOpciones.java
@@ -0,0 +1,45 @@
+package JuegoOpciones;
+
+import java.util.Scanner;
+
+public class JuegoHistoriaOpciones {
+
+	 static Scanner scanner = new Scanner(System.in);
+		public static void main(String[] args) {
+
+			System.out.println("Bienvenido a la historia donde tú eres el protagonista!");
+			System.out.println("");
+
+			System.out.println("Podrás elegir entre varias opciones para decidir el rumbo de tu aventura.");
+			System.out.println("");
+			
+			System.out.println("La historia comienza a las 9 de la mañana del 15 de diciembre de 1995 en el hospital Infanta Sofía de Madrid, "
+					+ "hacia un frio helador y aún no había salido el sol.");
+			System.out.println("Hoy era un día especial, Ingrid Baker estaba a punto de dar a luz. El médico anteriormente le había avisado que iba a tener dos mellizos, un niño y una niña.");
+			System.out.println("");
+			
+			System.out.println("Debido a esta situación, el padre; Richard Williams, decidió dejar a Ingrid y cortar el contacto con ella y sus futuros hijos.");
+			System.out.println("");
+
+			System.out.println("El embarazo iba muy bien, pero iba a suceder un imprevisto desafortunado que cambiaría drásticamente el devenir de la historia, "
+					+ "y eres tú quién debe decidir qué rumbo tomar. Este día marcará tu vida y a partir de ahí depende de ti y de tus actos en quién tú decidas ser.");
+			System.out.println("");
+			
+			System.out.println("Llega el momento de tomar tu primera decisión. Estas en la una encrucijada antes de nacer.");
+			System.out.println("");
+			
+			System.out.println("1.- Ingrid sobreviva al parto pero eso involucraría que tu hermano fallezca.");
+			System.out.println("2.- Decidir salvar a tu hermano, tendrá como consecuencia la muerte de Ingrid.");
+			int eleccion = scanner.nextInt();
+			
+			if (eleccion == 1){
+				
+				
+			} else {
+				
+				
+			}
+			
+		}
+
+}
